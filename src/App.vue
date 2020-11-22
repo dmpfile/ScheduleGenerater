5159723251c9797d6f4da296cc83e74477e36a7c
名久井涼佑 <nakuiryosuke@nakuiryouyuunoMacBook-Pro.local>
2020-11-22 16:39:43 +0900

    [update]App.vue：アサイドのリンク追加とログイン・ログアウトの処理追加

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,8 @@
 <template>
   <v-app>
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-      clipped
-    >
+    <v-navigation-drawer v-model="drawer" app clipped>
       <v-list dense>
-        <v-list-item link>
+        <v-list-item link to="/edit">
           <v-list-item-action>
             <v-icon>mdi-pencil</v-icon>
           </v-list-item-action>
@@ -14,7 +10,7 @@
             <v-list-item-title>Edit</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
-        <v-list-item link>
+        <v-list-item link to="/chart">
           <v-list-item-action>
             <v-icon>mdi-poll-box</v-icon>
           </v-list-item-action>
@@ -29,23 +25,18 @@
           <v-list-item-content>
             <v-list-item-title>Log Out</v-list-item-title>
           </v-list-item-content>
-        </v-list-item>            
+        </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    
-    <v-app-bar
-      app
-      clipped-left
-    >
+
+    <v-app-bar app clipped-left>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>Scedule Generetor</v-toolbar-title>
     </v-app-bar>
 
     <v-main>
       <v-row justify="center" align="center">
-        <v-col
-          cols=12 sm=10 md=8 lg=4 xl=3
-        >
+        <v-col cols="12" sm="10" md="8" lg="4" xl="3">
           <router-view></router-view>
         </v-col>
       </v-row>
@@ -73,15 +64,17 @@ export default {
       this.user = user ? user : {};
       if (user) {
         this.isLogin = true;
-        this.$router.push('/edit');
+        this.$router.push("/edit");
       }
     });
   },
   methods: {
     signOut() {
       firebase.auth().signOut();
-      this.$router.push('/login');
-    }
-  }
+      this.$gapi.logout();
+      this.$router.push("/login");
+      this.isLogin = false;
+    },
+  },
 };
 </script>
